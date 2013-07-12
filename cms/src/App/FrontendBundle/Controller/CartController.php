3965b038b3504e99c651b5ec382326648cbc34c8
Jakub Biernacki <jakub.biernacki@redexperts.net>
2013-07-12 15:26:45 +0200

    routing + controllers

--- cms/src/App/FrontendBundle/Controller/CartController.php
+++ cms/src/App/FrontendBundle/Controller/CartController.php
@@ -9,37 +9,45 @@
 
 namespace App\FrontendBundle\Controller;
 
+
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Response;
 
 class CartController extends Controller {
 
-
     protected $products = array();
 
-
     public function __construct() {
-        parent::__construct();
-
+        // load some products
         // key is a product ID
-
-        $this->products[0] = array('name' => 'ITEM1', 22.12);
-        $this->products[1] = array('name' => 'ITEM2', 10.05);
-        $this->products[2] = array('name' => 'ITEM3', 50.02);
-        $this->products[3] = array('name' => 'ITEM4', 102.12);
-        $this->products[5] = array('name' => 'ITEM4', 122.12);
+        $this->products[0] = array('name' => 'ITEM1', 'price' => 22.12);
+        $this->products[1] = array('name' => 'ITEM2', 'price' => 10.05);
+        $this->products[2] = array('name' => 'ITEM3', 'price' => 50.02);
+        $this->products[3] = array('name' => 'ITEM4', 'price' => 102.12);
+        $this->products[5] = array('name' => 'ITEM4', 'price' => 2.12);
     }
 
+
+    // puts one product to cart with $productID
     public function putAction($productID) {
+        $data = $this->get('liuggio_stats_d_client.factory')->produceStatsdData('cart.put');
+        $this->get('liuggio_stats_d_client.service')->send($data);
 
-//        if() {
-
-        }
-
+        return new Response(json_encode(array('id' => $productID, $this->products[$productID]['name'], 'price' => $this->products[$productID]['price'])));
     }
 
+    // removes all products from cart with $productID
+    public function removeAction($productID) {
+        $data = $this->get('liuggio_stats_d_client.factory')->produceStatsdData('cart.remove');
+        $this->get('liuggio_stats_d_client.service')->send($data);
 
-    public function removeAction() {
+        return new Response(json_encode(array($productID)));
+    }
 
+    //remove all products
+    public function clearAction() {
+        return new Response(json_encode(array(true)));
     }
 
 
