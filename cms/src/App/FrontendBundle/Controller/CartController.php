794f565bf42bfc5145d2b19a034b5a4e9ec299a9
Daniel Korsak <daniel.korsak@redexperts.net>
2013-07-12 15:39:17 +0200

    Updates

--- cms/src/App/FrontendBundle/Controller/CartController.php
+++ cms/src/App/FrontendBundle/Controller/CartController.php
@@ -10,44 +10,48 @@
 namespace App\FrontendBundle\Controller;
 
 
+use Symfony\Component\HttpFoundation\RedirectResponse;
+
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Response;
 
 class CartController extends Controller {
 
-    protected $products = array();
-
-    public function __construct() {
-        // load some products
-        // key is a product ID
-        $this->products[0] = array('name' => 'ITEM1', 'price' => 22.12);
-        $this->products[1] = array('name' => 'ITEM2', 'price' => 10.05);
-        $this->products[2] = array('name' => 'ITEM3', 'price' => 50.02);
-        $this->products[3] = array('name' => 'ITEM4', 'price' => 102.12);
-        $this->products[5] = array('name' => 'ITEM4', 'price' => 2.12);
-    }
-
-
     // puts one product to cart with $productID
-    public function putAction($productID) {
-        $data = $this->get('liuggio_stats_d_client.factory')->produceStatsdData('cart.put');
-        $this->get('liuggio_stats_d_client.service')->send($data);
+    public function putAction() 
+    {
+        $factory = $this->get('liuggio_stats_d_client.factory');
+        $statsdClient = $this->get('liuggio_stats_d_client.service');
 
-        return new Response(json_encode(array('id' => $productID, $this->products[$productID]['name'], 'price' => $this->products[$productID]['price'])));
+        $data = $factory->increment('aplikacja.demo.cart.put');
+        $statsdClient->send($data);
+
+        return new RedirectResponse($this->generateUrl('homepage'));
     }
 
     // removes all products from cart with $productID
-    public function removeAction($productID) {
-        $data = $this->get('liuggio_stats_d_client.factory')->produceStatsdData('cart.remove');
-        $this->get('liuggio_stats_d_client.service')->send($data);
+    public function removeAction() 
+    {
+        $factory = $this->get('liuggio_stats_d_client.factory');
+        $statsdClient = $this->get('liuggio_stats_d_client.service');
 
-        return new Response(json_encode(array($productID)));
+        $data = $factory->increment('aplikacja.demo.cart.remove');
+        $statsdClient->send($data);
+
+        return new RedirectResponse($this->generateUrl('homepage'));
     }
 
     //remove all products
-    public function clearAction() {
-        return new Response(json_encode(array(true)));
+    public function clearAction() 
+    {
+        $factory = $this->get('liuggio_stats_d_client.factory');
+        $statsdClient = $this->get('liuggio_stats_d_client.service');
+
+        $data = $factory->increment('aplikacja.demo.cart.clear');
+        $statsdClient->send($data);
+
+        return new RedirectResponse($this->generateUrl('homepage'));
     }
 
 
--- cms/src/App/FrontendBundle/Controller/HomepageController.php
+++ cms/src/App/FrontendBundle/Controller/HomepageController.php
@@ -8,6 +8,12 @@ class HomepageController extends Controller
 {
     public function indexAction()
     {
+        $factory = $this->get('liuggio_stats_d_client.factory');
+        $statsdClient = $this->get('liuggio_stats_d_client.service');
+
+        $data = $factory->increment('aplikacja.demo.visit.homepage');
+        $statsdClient->send($data);
+
         return $this->render('AppFrontendBundle:Homepage:index.html.twig');
     }
 }
